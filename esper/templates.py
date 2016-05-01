b67c80f9d33623d46f7f7389c8031caf14183e44
Benjamin Moran <benmoran56@gmail.com>
2016-05-01 13:49:42 +0900

    Minor work. Not functional.

--- esper/templates.py
+++ esper/templates.py
@@ -13,6 +13,8 @@ class ParallelProcessor(multiprocessing.Process):
     def __init__(self):
         super().__init__()
         self.world = None
+        self._local_ent = None
+        self._local_comp = None
         self.kill_switch = multiprocessing.Event()
         self.process_switch = multiprocessing.Event()
         self.sync_switch = multiprocessing.Event()
@@ -23,6 +25,10 @@ class ParallelProcessor(multiprocessing.Process):
 
     def run(self):
         print("Starting {},  pid: {}".format(self.name, self.pid))
+
+        self._local_ent = self.world._entities
+        self._local_comp = self.world._components
+
         while not self.kill_switch.is_set():
             # FIXME: skip processing if still busy.
             self.process_switch.wait()
